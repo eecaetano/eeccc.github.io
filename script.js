ba9aa9f56fd9e8104626d4c07e116f37e80c8b45
ANDERSON R OLIVEIRA <103950004+eecaetano@users.noreply.github.com>
2024-10-04 18:40:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,15 +20,16 @@ document.getElementById('verificationForm').addEventListener('submit', async fun
     if (user) {
         result.textContent = `Número de Registro Acadêmico: ${user["NÚMERO DO REGISTRO"]}`;
     } else {
-        result.textContent = 'Informações não encontradas ou incompatíveis';
-        result.textContent = 'VERIFIQUE OS DADOS INFORMADOS E TENTE NOVAMENTE.'
+        result.textContent = 'Informações não encontradas ou incompatíveis. VERIFIQUE OS DADOS INFORMADOS E TENTE NOVAMENTE.';
     }
-});
-     // Obtém a data e hora atuais
+
+    // Obtém a data e hora atuais
     const now = new Date();
     const date = now.toLocaleDateString('pt-BR');
     const time = now.toLocaleTimeString('pt-BR');
 
     // Exibe as informações do estudante e a data/hora da pesquisa
+    const studentInfo = `Nome: ${name}, Série: ${grade}, Turma: ${classGroup}`;
     document.getElementById('student-info').textContent = studentInfo;
     document.getElementById('search-datetime').textContent = `Pesquisa realizada no dia: ${date} / hora: ${time}`;
+});
